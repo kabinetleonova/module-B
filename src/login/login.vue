74bfff42ccb865954c4d4fc9e6f7eefee098acd5
kabinetleonova <kabinetleonova@bk.ru>
2024-03-20 12:53:44 +0300

    first commit

---
+++ src/login/login.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      login: "",
+      password: "",
+      error: ""
+    }
+  },
+  methods: {
+    postauth() {
+      if(this.login.trim().length < 3) {
+        this.error = "Логин должен быть более чем из 3 символов"
+      }
+
+      if(this.password.trim().length < 8) {
+        this.error = "Логин должен быть более чем из 3 символов"
+      }
+
+      this.error = ""
+
+      axios.post()
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <form>
+      <input v-model="login" placeholder="Логин">
+      <input v-model="password" placeholder="Пароль">
+      <button @click="postauth">Войти</button>
+    </form>
+
+    <p>{{ error }}</p>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
